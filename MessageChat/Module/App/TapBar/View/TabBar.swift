36cb8a9526e903bc213da3b369660375375b7206
David Kochkin <kochkin.david@gmail.com>
2024-08-16 19:54:02 +0300

    Add TabBar View

---
+++ MessageChat/Module/App/TapBar/View/TabBar.swift
@@ -0,0 +1,18 @@
+//
+//  TabBar.swift
+//  MessageChat
+//
+//  Created by David Kochkin on 16.08.2024.
+//
+
+import UIKit
+
+class TabBar: UITabBarController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
