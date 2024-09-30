24a574650476b675f408860643acf30680d126d4
David Kochkin <kochkin.david@gmail.com>
2024-09-30 18:30:36 +0300

    Add MessagesDataSource

--- MessageChat/Module/App/Messages/Messenger/Presenter/MessengerPresenter.swift
+++ MessageChat/Module/App/Messages/Messenger/Presenter/MessengerPresenter.swift
@@ -10,23 +10,26 @@ import MessageKit
 
 protocol MessengerPresenterProtocol: AnyObject {
     var title: String { get set }
-    
+    var selfSender: Sender { get set}
+    var messages: [Message] { get set }
 }
 
 class MessengerPresenter: MessengerPresenterProtocol {
+    var messages: [Message]
 
     weak var view: MessengerViewProtocol?
     private var convoId: String
     var title: String
     private var otherId: String
     
-    private lazy var selfSender = Sender(senderId: "1", displayName: "David")
+   lazy var selfSender = Sender(senderId: "1", displayName: "David")
     private lazy var otherSender = Sender(senderId: otherId, displayName: "Robert")
     
     init(view: MessengerViewProtocol?, convoId: String, otherId: String, name: String) {
-        self.view    = view
-        self.convoId = convoId
-        self.title   = name
-        self.otherId = otherId
+        self.view     = view
+        self.convoId  = convoId
+        self.title    = name
+        self.otherId  = otherId
+        self.messages = Message.mockData()
     }
 }
--- MessageChat/Module/App/Messages/Messenger/View/MessengerView.swift
+++ MessageChat/Module/App/Messages/Messenger/View/MessengerView.swift
@@ -12,11 +12,32 @@ protocol MessengerViewProtocol: AnyObject {
     
 }
 
-class MessengerView: MessagesViewController, MessengerViewProtocol {
+class MessengerView: MessagesViewController, MessengerViewProtocol, MessagesLayoutDelegate {
     var presenter: MessengerPresenterProtocol!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = presenter.title
+    }
+    
+    private func messengerSsetup() {
+        messagesCollectionView.messagesDataSource     = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messageCellDelegate    = self
+    }
+}
+
+
+extension MessengerView: MessagesDataSource {
+    var currentSender: SenderType {
+        presenter.selfSender
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessageKit.MessagesCollectionView) -> any MessageKit.MessageType {
+        presenter.messages[indexPath.section]
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessageKit.MessagesCollectionView) -> Int {
+        presenter.messages.count
     }
 }
